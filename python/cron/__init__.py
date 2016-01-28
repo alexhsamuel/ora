a9eda0576333de9f6ad7efd3ba1a6338dc5fe6d9
Alex Samuel <alex@alexsamuel.net>
2016-01-28 14:54:23 -0500

    Top-level module, includig ext imports, Weekday, Month, and MonthOfYear.

---
+++ python/cron/__init__.py
@@ -0,0 +1,96 @@
+import enum
+
+import pln.py
+
+from   ._ext import Date, SmallDate
+
+__all__ = (
+    "Date",
+    "Month",
+    "MonthOfYear",
+    "SmallDate",
+    "Weekday",
+
+    "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun",
+
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+    )
+
+#-------------------------------------------------------------------------------
+
+class Weekday(enum.IntEnum):
+    """
+    A day of the (seven-day) week.
+    """
+
+    Mon = 0
+    Tue = 1
+    Wed = 2
+    Thu = 3
+    Fri = 4
+    Sat = 5
+    Sun = 6
+
+    def __repr__(self):
+        return super().__str__()
+
+
+    def __str__(self):
+        return self.name
+
+
+
+# Add the days of the week to the module namespace.
+globals().update(Weekday.__members__)
+
+
+class MonthOfYear:
+    """
+    A calendar month in a specific year.
+    """
+
+    def __init__(self, year, month):
+        self.__year = year
+        self.__month = month
+
+
+    def __repr__(self):
+        return pln.py.format_ctor(self, self.__year, self.__month)
+
+
+    def __truediv__(self, day):
+        return Date(self.__year, self.__month, day)
+
+
+
+class Month(enum.IntEnum):
+
+    Jan =  1
+    Feb =  2
+    Mar =  3
+    Apr =  4
+    May =  5
+    Jun =  6
+    Jul =  7
+    Aug =  8
+    Sep =  9
+    Oct = 10
+    Nov = 11
+    Dec = 12
+
+    def __repr__(self):
+        return super().__str__()
+
+
+    def __str__(self):
+        return self.name
+
+
+    def __rtruediv__(self, year):
+        return MonthOfYear(year, self)
+
+
+
+# Add the months to the module namespace.
+globals().update(Month.__members__)
