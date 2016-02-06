ec84e4c69f55efce4b95401d5bce320ea41af99c
Alex Samuel <alex@alexsamuel.net>
2016-02-06 14:00:21 -0500

    Beginning of Python unit tests.

---
+++ python/cron/test/test_date_datenum.py
@@ -0,0 +1,37 @@
+import datetime
+
+import pytest
+
+import cron
+from   cron import *
+
+import data
+
+#-------------------------------------------------------------------------------
+
+def test_datenum0():
+    assert Date.MIN.datenum  == cron.DATENUM_MIN
+    assert (1/Jan/2).datenum ==   1
+    assert (1/Feb/1).datenum ==  31
+    assert (2/Jan/1).datenum == 365
+    assert Date.LAST.datenum == cron.DATENUM_LAST
+
+
+def test_datenum_round_trip():
+    """
+    Samples datenums, making sure a round trip through date is idempotent.
+    """
+    for datenum in range(cron.DATENUM_MIN, cron.DATENUM_MAX, 37):
+        assert Date.from_datenum(datenum).datenum == datenum
+
+
+def test_vs_toordinal():
+    """
+    Tests that datenum is one less than datetime.date's (1-indexed) ordinal.
+    """
+    for parts in data.TEST_DATE_PARTS:
+        datenum = Date.from_parts(*parts).datenum
+        ordinal = datetime.date(*parts).toordinal()
+        assert datenum + 1 == ordinal
+
+
---
+++ python/cron/test/test_date_parts.py
@@ -0,0 +1,39 @@
+import datetime
+
+import pytest
+
+import cron
+from   cron import *
+
+import data
+
+#-------------------------------------------------------------------------------
+
+def test_parts_attrs():
+    for year, month, day in data.TEST_DATE_PARTS:
+        date = Date.from_parts(year, month, day)
+        assert date.valid
+        assert date.year == year
+        assert date.month == month
+        assert date.day == day
+
+
+def test_parts():
+    for year, month, day in data.TEST_DATE_PARTS:
+        parts = Date.from_parts(year, month, day).parts
+        assert parts.year == year
+        assert parts.month == month
+        assert parts.day == day
+        assert parts[: 3] == (year, month, day)
+
+
+def test_vs_date():
+    for parts in data.TEST_DATE_PARTS:
+        date = Date.from_parts(*parts)
+        ref = datetime.date(*parts)
+        assert date.year == ref.year
+        assert date.month == ref.month
+        assert date.day == ref.day
+
+
+
