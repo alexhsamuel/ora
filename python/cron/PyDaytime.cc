63804bbcc251beceef14face4ae510e16abfafc4
Alex Samuel <alex@alexsamuel.net>
2016-02-17 00:29:57 -0500

    Missing source file.

---
+++ python/cron/PyDaytime.cc
@@ -0,0 +1,41 @@
+#include <Python.h>
+
+#include "py.hh"
+
+#include "PyDaytime.hh"
+
+namespace alxs {
+
+//------------------------------------------------------------------------------
+
+StructSequenceType*
+get_daytime_parts_type()
+{
+  static StructSequenceType type;
+
+  if (type.tp_name == nullptr) {
+    // Lazy one-time initialization.
+    static PyStructSequence_Field fields[] = {
+      {(char*) "hour"       , nullptr },
+      {(char*) "minute"     , nullptr },
+      {(char*) "second"     , nullptr },
+      {nullptr, nullptr}
+    };
+    static PyStructSequence_Desc desc{
+      (char*) "DaytimeParts",                               // name
+      nullptr,                                              // doc
+      fields,                                               // fields
+      3                                                     // n_in_sequence
+    };
+
+    StructSequenceType::InitType(&type, &desc);
+  }
+
+  return &type;
+}
+
+
+//------------------------------------------------------------------------------
+
+}  // namespace alxs
+
