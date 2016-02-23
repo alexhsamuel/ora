27114e45a3f216ffa80755a64140dfc2b66a6467
Alex Samuel <alex@alexsamuel.net>
2016-02-23 15:32:55 -0500

    Set ZONEINFO.

--- env.sh
+++ env.sh
@@ -1,5 +1,6 @@
 . $(dirname $BASH_ARGV)/cfg.sh
 export PATH=$PYTHON_HOME/bin:$PATH
 export PYTHONPATH=$PLYNTH_HOME:$CRON_HOME/python
+export ZONEINFO=$CRON_HOME/share/zoneinfo
 function py3() { $PYTHON_HOME/bin/python3 -q "$@"; }
 
