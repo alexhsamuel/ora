e617e8a80a75e2f153047fc570de5886d5b12e3a
Alex Samuel <alex@alexsamuel.net>
2016-03-02 00:29:51 -0500

    Some tests.

--- python/cron/test/test_from_local.py
+++ python/cron/test/test_from_local.py
@@ -11,13 +11,22 @@ import data
 def test_utc0():
     t = from_local((1973/Dec/3, 7200.125), UTC)
     # FIXME
-    assert(str(t) == "1973-12-03T02:00:00.12500000Z")
+    assert(str(t) == "1973-12-03T02:00:00.1250000Z")
 
 
 def test_nsec_time():
-    t = from_local((1973/Dec/3, 7200.125), UTC, Time=NsecTime)
     # FIXME
-    assert(str(t) == "1973-12-03T02:00:00.1250000000Z")
+    t = from_local((1900/Jan/1, 0), UTC, Time=NsecTime)
+    assert str(t) == "1900-01-01T00:00:00.000000000Z"
+
+    t = from_local((2444/May/29, Daytime.from_parts(1, 53, 4)), UTC, Time=NsecTime)
+    assert str(t) == "2444-05-29T01:53:04.000000000Z"
+
+    t = from_local((2444/May/29, Daytime.from_parts(1, 53, 5)), UTC, Time=NsecTime)
+    assert str(t) == "INVALID"
+
+    t = from_local((1973/Dec/3, 7200.125), UTC, Time=NsecTime)
+    assert str(t) == "1973-12-03T02:00:00.125000000Z"
 
 
     
