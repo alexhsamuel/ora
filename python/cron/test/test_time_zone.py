8e0bf0f168427f27d3da81b3b1c922dee82b87b6
Alex Samuel <alex@alexsamuel.net>
2016-03-17 12:01:31 -0400

    Time zone tests.

--- python/cron/test/test_time_zone.py
+++ python/cron/test/test_time_zone.py
@@ -10,7 +10,25 @@ def test_utc():
 
 
 def test_name():
-    assert TimeZone.get("US/Eastern").name == "US/Eastern"
+    assert TimeZone("US/Eastern").name == "US/Eastern"
 
+
+def test_us_central():
+    tz = TimeZone("US/Central")
+    H = 3600
+    assert tz.at_local(1960/Jan/ 1, MIDNIGHT) == (-6 * H, "CST", False)
+    assert tz.at_local(1960/Jul/ 1, MIDNIGHT) == (-5 * H, "CDT", True)
+    assert tz.at_local(1969/Jan/ 1, MIDNIGHT) == (-6 * H, "CST", False)
+    assert tz.at_local(1969/Jul/ 1, MIDNIGHT) == (-5 * H, "CDT", True)
+    assert tz.at_local(1970/Jan/ 1, MIDNIGHT) == (-6 * H, "CST", False)
+    assert tz.at_local(1970/Jul/ 1, MIDNIGHT) == (-5 * H, "CDT", True)
+    assert tz.at_local(1971/Jan/ 1, MIDNIGHT) == (-6 * H, "CST", False)
+    assert tz.at_local(1971/Jul/ 1, MIDNIGHT) == (-5 * H, "CDT", True)
+    assert tz.at_local(1980/Jan/ 1, MIDNIGHT) == (-6 * H, "CST", False)
+    assert tz.at_local(1980/Jul/ 1, MIDNIGHT) == (-5 * H, "CDT", True)
+    assert tz.at_local(2000/Jan/ 1, MIDNIGHT) == (-6 * H, "CST", False)
+    assert tz.at_local(2000/Jul/ 1, MIDNIGHT) == (-5 * H, "CDT", True)
+    assert tz.at_local(2020/Jan/ 1, MIDNIGHT) == (-6 * H, "CST", False)
+    assert tz.at_local(2020/Jul/ 1, MIDNIGHT) == (-5 * H, "CDT", True)
 
 
