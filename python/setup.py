93c559e21f55449c549d7c94244eaca2359ee0d3
Alex Samuel <alex@alexsamuel.net>
2016-03-20 10:12:16 -0400

    Use numpu.distutils.

--- python/setup.py
+++ python/setup.py
@@ -4,21 +4,37 @@ from   setuptools import setup, Extension
 import sys
 
 if sys.platform == "darwin":
-  # No C++14 when building for earlier OSX versions.
-  os.environ["MACOSX_DEPLOYMENT_TARGET"] = "10.9"
+    # No C++14 when building for earlier OSX versions.
+    os.environ["MACOSX_DEPLOYMENT_TARGET"] = "10.9"
+
+include_dirs = ["../include", ]
+
+#-------------------------------------------------------------------------------
+
+try:
+    import numpy
+except ImportError:
+    have_numpy = False
+    print("no numpy found; building without")
+else:
+    have_numpy = True
+    from numpy.distutils.misc_util import get_numpy_include_dirs
+    include_dirs.extend(get_numpy_include_dirs())
+
+#-------------------------------------------------------------------------------
 
 setup(
-  name="test",
-  ext_modules=[
-    Extension(
-      "cron._ext",
-      extra_compile_args=["-std=c++14", ],
-      include_dirs      =["../include", ],
-      sources           =glob("cron/*.cc"),
-      library_dirs      =["../lib",],
-      libraries         =["cron",],
-      depends           =glob("cron/*.hh") + glob("../include/*.hh"),
-    ),
+    name="test",
+    ext_modules=[
+        Extension(
+            "cron._ext",
+            extra_compile_args=["-std=c++14", ],
+            include_dirs      =include_dirs,
+            sources           =glob("cron/*.cc"),
+            library_dirs      =["../lib",],
+            libraries         =["cron",],
+            depends           =glob("cron/*.hh") + glob("../include/*.hh"),
+        ),
 
   ]
 )
