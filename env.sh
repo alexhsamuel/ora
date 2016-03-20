b1c93079a0e3b722a89eea04074898264796a6bc
Alex Samuel <alex@alexsamuel.net>
2016-03-20 10:11:28 -0400

    Use supdoc.

--- env.sh
+++ env.sh
@@ -1,6 +1,8 @@
 . $(dirname $BASH_ARGV)/cfg.sh
 export PATH=$PYTHON_HOME/bin:$PATH
-export PYTHONPATH=$PLYNTH_HOME:$CRON_HOME/python
+export PYTHONPATH=$PLYNTH_HOME:$CRON_HOME/python:$SUPDOC_HOME/src/python
 export PYTHONSTARTUP=$CRON_HOME/pythonstartup
 export ZONEINFO=$CRON_HOME/share/zoneinfo
 function py3() { $PYTHON_HOME/bin/python3 -q "$@"; }
+function supdoc() { clear; py3 -m supdoc.cli "$@" | less -eF; }
+
