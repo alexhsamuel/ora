0373824f7d7e7fb016ae183067ea4e79e8a18389
Alex Samuel <alex@alexsamuel.net>
2016-03-25 00:15:53 -0400

    Collapse datenum_to_parts().

--- src/date_math.cc
+++ src/date_math.cc
@@ -174,126 +174,16 @@ DateParts
 datenum_to_parts(
   Datenum const datenum)
 {
-  DateParts parts;
-
   if (! datenum_is_valid(datenum)) 
     return DateParts::get_invalid();
 
-  // Compute the 400-year leap cycle and remainder; count from year 1.
-  parts.year = 1 + 400 * (datenum / 146097);
-  uint32_t rem = datenum % 146097;
+  auto const ord = datenum_to_ordinal_date(datenum);
+  auto const ymd = datenum_to_ymd(datenum, ord);
+  auto const wdy = get_weekday(datenum);
+  auto const wdt = datenum_to_week_date(datenum, ord, wdy);
 
-  // Adjust for the 100-year leap cycle and remainder.
-  if (rem == 146096) {
-    parts.year += 300;
-    rem = 36524;
-  }
-  else {
-    parts.year += 100 * (rem / 36524);
-    rem %= 36524;
-  }
-
-  // Adjust for the 4-year leap cycle and remainder.
-  parts.year += 4 * (rem / 1461);
-  rem %= 1461;
-
-  // Compute the one-year cycle and remainder.
-  // FIXME: Probably wrong.  Validate carefully.
-  if (rem == 1460) {
-    parts.year += 3;
-    rem = 365;
-  }
-  else {
-    parts.year += rem / 365;
-    rem %= 365;
-  }
-
-  parts.ordinal = rem;
-  parts.weekday = get_weekday(datenum);
-
-  auto const leap = is_leap_year(parts.year);
-
-  if (rem < 31) {
-    parts.month = 0;
-    parts.day = rem;
-  } 
-  else if (rem < 59 || (leap && rem == 59)) {
-    parts.month = 1;
-    parts.day = rem - 31;
-  }
-  else {
-    if (leap)
-      --rem;
-    if (rem < 90) {
-      parts.month = 2;
-      parts.day = rem - 59;
-    }
-    else if (rem < 120) {
-      parts.month = 3;
-      parts.day = rem - 90;
-    }
-    else if (rem < 151) {
-      parts.month = 4;
-      parts.day = rem - 120;
-    }
-    else if (rem < 181) {
-      parts.month = 5;
-      parts.day = rem - 151;
-    }
-    else if (rem < 212) {
-      parts.month = 6;
-      parts.day = rem - 181;
-    }
-    else if (rem < 243) {
-      parts.month = 7;
-      parts.day = rem - 212;
-    }
-    else if (rem < 273) {
-      parts.month = 8;
-      parts.day = rem - 243;
-    }
-    else if (rem < 304) {
-      parts.month = 9;
-      parts.day = rem - 273;
-    }
-    else if (rem < 334) {
-      parts.month = 10;
-      parts.day = rem - 304;
-    }
-    else {
-      parts.month = 11;
-      parts.day = rem - 334;
-    }
-  }
-
-  // The week number is the week number of the nearest Thursday.
-  int16_t const thursday = parts.ordinal + THURSDAY - parts.weekday;
-  if (thursday < 0) {
-    // The nearest Thursday is part of the previous week year.
-    parts.week_year = parts.year - 1;
-    // Calculate the week number of the previous December 31.  This calculation
-    // relies on the fact that in this case, the previous December 31 must be:
-    //   - a Thursday, in week 52
-    //   - a Friday, in week 52 of a leap year or week 51 otherwise, 
-    //   - a Saturday, in week 51.
-    Weekday const dec31_weekday = parts.weekday - parts.day - 1;
-    parts.week = 
-      (dec31_weekday == THURSDAY
-       || (dec31_weekday == FRIDAY && is_leap_year(parts.week_year)))
-      ? 52 : 51;
-  }
-  else if (thursday >= 365 && (thursday >= 366 || ! is_leap_year(parts.year))) {
-    // The nearest Thursday is part of the next week year.
-    parts.week_year = parts.year + 1;
-    parts.week = 0;
-  }
-  else {
-    parts.week_year = parts.year;
-    // Just count Thursdays.
-    parts.week = thursday / 7;
-  }
-
-  return parts;
+  return DateParts{
+    ymd.year, ymd.month, ymd.day, ord.ordinal, wdt.week_year, wdt.week, wdy};
 }
 
 
