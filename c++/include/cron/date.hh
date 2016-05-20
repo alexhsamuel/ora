2ad46239739eafe98298123bb91ad9aabbc518a6
Alex Samuel <alex@alexsamuel.net>
2016-05-19 23:46:51 -0400

    Clean up.

--- c++/include/cron/date.hh
+++ c++/include/cron/date.hh
@@ -1,23 +1,6 @@
 #pragma once
 
-#include <iostream>  // FIXME: Remove.
-#include <limits>
-#include <string>
-
-#include "aslib/exc.hh"
-#include "aslib/math.hh"
-#include "cron/date_math.hh"
 #include "cron/date_functions.hh"
 #include "cron/date_safe.hh"
 #include "cron/date_obj.hh"
-#include "cron/types.hh"
-
-namespace cron {
-namespace date {
-
-//------------------------------------------------------------------------------
-
-}  // namespace date
-}  // namespace cron
-
 
