5bae86f5fb2f0c40d8f631c2333a1efeba96c877
Alex Samuel <alex@alexsamuel.net>
2016-06-05 15:28:17 -0400

    Function to build test programs.

--- env.sh
+++ env.sh
@@ -6,3 +6,12 @@ export ZONEINFO=$CRON_HOME/share/zoneinfo
 function py3() { $PYTHON_HOME/bin/python3 -q "$@"; }
 function supdoc() { clear; py3 -m supdoc.cli "$@" | less -eF; }
 
+function cron-build() { 
+    local src="$1"
+    local prog="${src%.cc}"
+    c++ -std=c++14 -Wall \
+         -I $CRON_HOME/c++/include \
+         "$src" $CRON_HOME/c++/src/libcron.a \
+         -o "$prog"
+}
+
