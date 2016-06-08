301d466513a864393dfe476d7fe1a6de5be04b3a
Alex Samuel <alex@alexsamuel.net>
2016-06-08 08:23:56 -0400

    Tests for ordinal_date and week_date.

--- python/cron/test/test_date_parts.py
+++ python/cron/test/test_date_parts.py
@@ -17,21 +17,13 @@ def test_from_ymd():
         assert not date.missing
 
 
-def test_parts_attrs():
+def test_ymd():
     for year, month, day in data.TEST_DATE_PARTS:
-        date = Date.from_ymd(year, month, day)
-        assert date.year == year
-        assert date.month == month
-        assert date.day == day
-
-
-def test_parts():
-    for year, month, day in data.TEST_DATE_PARTS:
-        parts = Date.from_ymd(year, month, day).parts
-        assert parts.year   == year
-        assert parts.month  == month
-        assert parts.day    == day
-        assert parts[: 3]   == (year, month, day)
+        ymd = Date.from_ymd(year, month, day).ymd
+        assert ymd.year   == year
+        assert ymd.month  == month
+        assert ymd.day    == day
+        assert ymd        == (year, month, day)
 
 
 def test_vs_date():
@@ -56,5 +48,46 @@ def test_weekday_vs_date():
         date = Date.from_ymd(*parts)
         ref = datetime.date(*parts)
         assert date.weekday == ref.weekday()
+
+
+def test_ordinal_date():
+    for date, od in (
+        (Date.MIN   , (   1,   1)),
+        (2000/Jan/ 1, (2000,   1)),
+        (2016/Jun/ 8, (2016, 160)),
+        (2204/Dec/31, (2204, 366)),
+        (Date.MAX   , (9999, 365)),
+    ):
+        assert date.ordinal_date == od
+        assert date.year == od[0]
+        assert date.ordinal_date.year == od[0]
+        assert date.ordinal == od[1]
+        assert date.ordinal_date.ordinal == od[1]
+
+        assert Date.from_ordinal_date(od) == date
+        assert Date.from_ordinal_date(*od) == date
+
+
+def test_week_date():
+    for date, wd in (
+        (Date.MIN   , (   1,  1, Mon)),
+        (2000/Jan/ 1, (1999, 52, Sat)),
+        (2000/Jan/ 2, (1999, 52, Sun)),
+        (2000/Jan/ 3, (2000,  1, Mon)),
+        (2016/Jun/ 8, (2016, 23, Wed)),
+        (2204/Dec/30, (2204, 52, Sun)),
+        (2204/Dec/31, (2205,  1, Mon)),
+        (Date.MAX   , (9999, 52, Fri)),
+    ):
+        assert date.week_date == wd
+        assert date.week_year == wd[0]
+        assert date.week_date.week_year == wd[0]
+        assert date.week == wd[1]
+        assert date.week_date.week == wd[1]
+        assert date.weekday == wd[2]
+        assert date.week_date.weekday == wd[2]
+
+        assert Date.from_week_date(wd) == date
+        assert Date.from_week_date(*wd) == date
 
 
