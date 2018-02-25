c04b57f37f296fb33e837b365c12b02c00986eb0
Alex Samuel <alex@alexsamuel.net>
2018-02-25 12:08:40 -0500

    Bump version: 0.2.1 → 0.2.2

--- python/ora/__init__.py
+++ python/ora/__init__.py
@@ -8,7 +8,7 @@ import warnings
 
 from   .ext import *
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 __all__ = (
     "Date",
--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ def enumerate_data_files(dir):
 
 setup(
     name            ="ora",
-    version         ="0.2.1",
+    version         ="0.2.2",
     description     ="Alternative time and date library",
     long_description=__doc__,
     url             ="https://github.com/alexhsamuel/ora",
