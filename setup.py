1784c9e028ea75d45dd1dc2e2b8e61b5738483e1
Alex Samuel <alex@alexsamuel.net>
2018-03-09 00:17:10 -0500

    Adjust package description.

--- setup.py
+++ setup.py
@@ -1,20 +1,11 @@
 """
-Ora is a coherent and high-perforamance C++ and Python 3 library for times,
-dates, time zones, and related concepts.  The central concept is a
-location-independent instant of time.  Local date and time of day
-representations are derived from this, using the (included) zoneinfo database.
-Multiple resolutions are provided for all time types.
+Ora is a freestanding time and date implementation for C++ and Python.
 
-.. code:: py
+Ora is `hosted on GitHub <http://github.com/alexhsamuel/ora>`_.  See
+the `installation instructions
+<https://github.com/alexhsamuel/ora/blob/master/README.md#installation>`_.
 
-    >>> import ora
-    >>> time = ora.now()
-    >>> print(time)
-    2017-12-26T03:47:36.41359399Z
-    >>> tz = ora.TimeZone("US/Eastern")
-    >>> (time @ tz).date
-    Date(2017, Dec, 25)
-
+Docs at `readthedocs <http://ora.readthedocs.io/en/latest/>`_.
 """
 
 #-------------------------------------------------------------------------------
