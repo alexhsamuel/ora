5e662b572cabaa92a6e8a409824e4e89de4783f9
Alex Samuel <alex@alexsamuel.net>
2018-07-03 15:16:33 -0600

    Modernize env.sh.

--- env.sh
+++ env.sh
@@ -1,17 +1,14 @@
-. $(dirname $BASH_ARGV)/cfg.sh
-export PATH=$PYTHON_HOME/bin:$PATH
-export PYTHONPATH=$CRON_HOME/python:$SUPDOC_HOME/src/python
-export PYTHONSTARTUP=$CRON_HOME/pythonstartup
-export ZONEINFO=$CRON_HOME/share/zoneinfo
-function py3() { $PYTHON_HOME/bin/python3 -q "$@"; }
+path PY ++ python
+
+function c++() { /usr/bin/c++ -std=c++14 -Wall "$@"; }
 function supdoc() { clear; py3 -m supdoc.cli "$@" | less -eF; }
 
-function cron-build() { 
+function ora-build() { 
     local src="$1"
     local prog="${src%.cc}"
-    c++ -std=c++14 -Wall \
-         -I $CRON_HOME/c++/include \
-         "$src" $CRON_HOME/c++/src/libcron.a \
-         -o "$prog"
+    c++ -I $HOME/dev/ora/cxx/include \
+        "$src" \
+        $CRON_HOME/cxx/src/libora.a \
+        -o "$prog"
 }
 
