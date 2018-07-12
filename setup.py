0f04ec5dc06810bf75bb0b0a22dd7b49bb1c472f
Alex Samuel <alex@alexsamuel.net>
2018-07-12 13:52:19 -0500

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ def enumerate_data_files(dir):
 
 setup(
     name            ="ora",
-    version         ="0.3.1",
+    version         ="0.3.2",
     description     ="Alternative time and date library",
     long_description=__doc__,
     url             ="https://github.com/alexhsamuel/ora",
