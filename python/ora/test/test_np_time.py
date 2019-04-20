644b6e3cb2fbe99bd0ebeebc422fd493efd7c302
Alex Samuel <alex@alexsamuel.net>
2019-04-20 09:25:00 -0400

    Reorg tests.

--- python/ora/test/test_np_time.py
+++ python/ora/test/test_np_time.py
@@ -1,6 +1,7 @@
 import numpy as np
 import ora
-from   ora import now, UTC
+from   ora import now, UTC, Time, Unix32Time
+import ora.np
 import pytest
 
 pytest.importorskip("ora.np")
@@ -159,5 +160,26 @@ def test_convert_invalid():
         ora.Date(2019, 4, 16),
         ora.Daytime(12, 30, 45),
     ], dtype=ora.Time) == ora.Time.INVALID).all()
+
+
+def test_time_from_offset():
+    offset = (1 << np.arange(30)).reshape(3, 5, 2)
+    time = ora.np.time_from_offset(offset, Time=Unix32Time)
+    assert time.shape == (3, 5, 2)
+    assert time.dtype == Unix32Time.dtype
+    assert time[0][0][0] == Unix32Time.from_offset(1)
+    assert time[2][4][1] == Unix32Time.from_offset(1 << 29)
+
+
+def test_time_from_offset_cast():
+    offset = np.array([0, 1, 60], dtype="uint32") * Time.DENOMINATOR
+    time = ora.np.time_from_offset(offset)
+    assert time.shape == (3, )
+    assert time.dtype == Time.dtype
+    assert all(time == [
+        Time(1, 1, 1, 0, 0, 0, UTC),
+        Time(1, 1, 1, 0, 0, 1, UTC),
+        Time(1, 1, 1, 0, 1, 0, UTC),
+    ])
 
 
