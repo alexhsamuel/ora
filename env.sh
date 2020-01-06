d256190110e1357d2250eae18ad8561fef9aa59a
Alex Samuel <alex@alexsamuel.net>
2020-01-06 16:39:12 +0700

    Clean up.

--- env.sh
+++ env.sh
@@ -1,14 +1,5 @@
-path PY ++ python
+path PY ++ $(dirname ${BASH_SOURCE[0]})/python
 
 function c++() { /usr/bin/c++ -std=c++14 -Wall "$@"; }
 function supdoc() { clear; py3 -m supdoc.cli "$@" | less -eF; }
-
-function ora-build() { 
-    local src="$1"
-    local prog="${src%.cc}"
-    c++ -I $HOME/dev/ora/cxx/include \
-        "$src" \
-        $CRON_HOME/cxx/src/libora.a \
-        -o "$prog"
-}
 
