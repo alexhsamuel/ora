caaba8095cdadbc76d02516cbb3801357e8564be
Alex Samuel <alex@alexsamuel.net>
2022-09-02 09:48:43 -0400

    Rounding in TimeFmt.

--- python/ora/test/test_fmt.py
+++ python/ora/test/test_fmt.py
@@ -1,6 +1,7 @@
+import numpy as np
 import pytest
 
-from   ora import Date, Date16, DateFmt, TimeFmt, TIME_TYPES
+from   ora import Date, Date16, Time128, DateFmt, TimeFmt, TIME_TYPES
 
 #-------------------------------------------------------------------------------
 
@@ -32,6 +33,53 @@ def test_time_fmt():
     fmt = TimeFmt(4)
     assert fmt("2022-08-23T00:07:52.123Z") == "2022-08-23T00:07:52.1230+00:00"
     assert fmt("2022-08-23T00:07:52.789Z") == "2022-08-23T00:07:52.7890+00:00"
+
+    fmt = TimeFmt(8)
+    assert fmt("2022-08-23T00:07:52.123Z") == "2022-08-23T00:07:52.12300000+00:00"
+    assert fmt("2022-08-23T00:07:52.789Z") == "2022-08-23T00:07:52.78900000+00:00"
+
+    fmt = TimeFmt(9)
+    assert fmt("2022-08-23T00:07:52.123Z") == "2022-08-23T00:07:52.123000000+00:00"
+    assert fmt("2022-08-23T00:07:52.789Z") == "2022-08-23T00:07:52.789000000+00:00"
+    assert fmt("2022-08-23T00:07:52.123456789Z") == "2022-08-23T00:07:52.123456789+00:00"
+
+
+def test_time_fmt_datetime64():
+    t = np.datetime64("2022-09-01T01:23:45.345678901", "ns")
+    assert TimeFmt(-1)(t) == "2022-09-01T01:23:45+00:00"
+    assert TimeFmt( 0)(t) == "2022-09-01T01:23:45.+00:00"
+    assert TimeFmt( 1)(t) == "2022-09-01T01:23:45.3+00:00"
+    assert TimeFmt( 2)(t) == "2022-09-01T01:23:45.35+00:00"
+    assert TimeFmt( 3)(t) == "2022-09-01T01:23:45.346+00:00"
+    assert TimeFmt( 4)(t) == "2022-09-01T01:23:45.3457+00:00"
+    assert TimeFmt( 5)(t) == "2022-09-01T01:23:45.34568+00:00"
+    assert TimeFmt( 6)(t) == "2022-09-01T01:23:45.345679+00:00"
+    assert TimeFmt( 7)(t) == "2022-09-01T01:23:45.3456789+00:00"
+    assert TimeFmt( 8)(t) == "2022-09-01T01:23:45.34567890+00:00"
+    assert TimeFmt( 9)(t) == "2022-09-01T01:23:45.345678901+00:00"
+    assert TimeFmt(10)(t) == "2022-09-01T01:23:45.3456789010+00:00"
+
+
+def test_time_fmt_128():
+    t = Time128("2022-09-01T12:34:56.789012345678+00:00")
+    assert TimeFmt(-1)(t) == "2022-09-01T12:34:57+00:00"
+    assert TimeFmt( 0)(t) == "2022-09-01T12:34:57.+00:00"
+    assert TimeFmt( 1)(t) == "2022-09-01T12:34:56.8+00:00"
+    assert TimeFmt( 2)(t) == "2022-09-01T12:34:56.79+00:00"
+    assert TimeFmt( 3)(t) == "2022-09-01T12:34:56.789+00:00"
+    assert TimeFmt( 4)(t) == "2022-09-01T12:34:56.7890+00:00"
+    assert TimeFmt( 5)(t) == "2022-09-01T12:34:56.78901+00:00"
+    assert TimeFmt( 6)(t) == "2022-09-01T12:34:56.789012+00:00"
+    assert TimeFmt( 7)(t) == "2022-09-01T12:34:56.7890123+00:00"
+    assert TimeFmt( 8)(t) == "2022-09-01T12:34:56.78901235+00:00"
+    assert TimeFmt( 9)(t) == "2022-09-01T12:34:56.789012346+00:00"
+
+
+# Currently doesn't work because we use NsTime in the implementation.
+@pytest.mark.xfail
+def test_time_fmt_128hi():
+    t = Time128("2022-09-01T12:34:56.789012345678+00:00")
+    assert TimeFmt(10)(t) == "2022-09-01T12:34:56.7890123457+00:00"
 
 
 @pytest.mark.parametrize("Time", TIME_TYPES)
