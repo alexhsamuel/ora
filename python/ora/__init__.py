f2d64651bd4cedcaf8341ee5e70b159d198e7cc4
Alex Samuel <alex@alexsamuel.net>
2023-04-09 14:56:15 -0400

    Bump version: 0.8.0 → 0.8.1

--- python/ora/__init__.py
+++ python/ora/__init__.py
@@ -13,7 +13,7 @@ from   .ext import *
 from   .weekday import *
 from   .util import Range
 
-__version__ = "0.8.0"
+__version__ = "0.8.1"
 
 __all__ = (
     "Calendar",
--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ def enumerate_data_files(dir):
 
 setup(
     name            ="ora",
-    version         ="0.8.0",
+    version         ="0.8.1",
     description     ="Alternative time and date library",
     long_description=__doc__,
     url             ="https://github.com/alexhsamuel/ora",
