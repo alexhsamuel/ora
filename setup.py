f1426a1b25da0ffb50fc7794d108b01c3c64de6b
Alex Samuel <alex@alexsamuel.net>
2025-11-27 09:37:55 -0500

    Bump version: 0.9.1 → 0.10.0

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ class BuildExt(setuptools.command.build_ext.build_ext):
 
 setup(
     name            ="ora",
-    version         ="0.9.1",
+    version         ="0.10.0",
     description     ="Alternative time and date library",
     long_description=__doc__,
     url             ="https://github.com/alexhsamuel/ora",
